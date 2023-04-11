cafe77522c97f9251c10bf5edc8241b3f3f6ca1a
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-04-11 17:57:50 +0800

    Create eligibleToVote.py

---
+++ eligibleToVote.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+if age <18:
+    print("Sorry " + name + " You are not eligible to vote :<")
+else:
+    print("Congrats " + name + " You are eligible to vote :>")
