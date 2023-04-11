edfc54184c16f312a8f22dc5d38bc2017afb7cd1
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-04-11 17:30:45 +0800

    Create (1 - 26) AlphabetNumberSystem.py

---
+++ (1 - 26) AlphabetNumberSystem.py
@@ -0,0 +1,59 @@
+# 1 - 26 Alphabet Number System
+# a program that will ask the user to choose between 1 to 26 then
+# display the corresponding letter that in the Alphabet based on the number chosen.
+
+enter = int(input("Enter a number 1 - 26: "))
+if(enter == 1):
+    print("A")
+elif(enter == 2):
+    print("B")
+elif(enter == 3):
+    print("C")
+elif(enter == 4):
+    print("D")
+elif(enter == 5):
+    print("E")
+elif(enter == 6):
+    print("F")
+elif(enter == 7):
+    print("G")
+elif(enter == 8):
+    print("H")
+elif(enter == 9):
+    print("I")
+elif(enter == 10):
+    print("J")
+elif(enter == 11):
+    print("K")
+elif(enter == 12):
+    print("L")
+elif(enter == 13):
+    print("M")
+elif(enter == 14):
+    print("N")
+elif(enter == 15):
+    print("O")
+elif(enter == 16):
+    print("P")
+elif(enter == 17):
+    print("Q")
+elif(enter == 18):
+    print("R")
+elif(enter == 19):
+    print("S")
+elif(enter == 20):
+    print("T")
+elif(enter == 21):
+    print("U")
+elif(enter == 22):
+    print("V")
+elif(enter == 23):
+    print("W")
+elif(enter == 24):
+    print("X")
+elif(enter == 25):
+    print("Y")
+elif(enter == 26):
+    print("Z")
+else:
+    print("Wrong number input")
