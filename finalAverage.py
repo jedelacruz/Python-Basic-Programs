f41b852ded28581e2838dd5c4bb37621985081bf
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-04-11 17:36:34 +0800

    Create finalAverage.py

---
+++ finalAverage.py
@@ -0,0 +1,10 @@
+Math = float(input("Enter your grade in Math: "))
+Science = float(input("Enter your grade in Science: "))
+English = float(input("Enter your grade in English: "))
+Filipino = float(input("Enter your grade in Filipino: "))
+Computer = float(input("Enter your grade in Computer: "))
+
+results = Math + Science + English + Filipino +  Computer
+FinalGrade = results/5
+
+print(FinalGrade)
