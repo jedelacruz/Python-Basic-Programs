076edc4e9e1797b04c925a603725303a62a4f1b0
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-07-30 12:51:46 +0800

    Create positiveOrNegative.py

---
+++ positiveOrNegative.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+
+if number > 0:
+    print(f"Your {number} is a positive number")
+elif number == 0:
+    print(f"Your {number} is 0")
+else:
+    print(f"Your {number} is a negative number")
