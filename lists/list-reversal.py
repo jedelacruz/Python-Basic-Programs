62d94f44192d2ff8cdd2347e4da1f5065412442f
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-09-01 11:56:51 +0800

    Create list-reversal.py

---
+++ lists/list-reversal.py
@@ -0,0 +1,13 @@
+"""
+3. List Reversal:
+Write a program that takes a list as input and prints the list in reverse order.
+"""
+list = [70, 40, 50]
+
+print("List is")
+for i in list:
+    print(i)
+
+print("Reverse list is")
+for i in reversed(list):
+    print(i)
