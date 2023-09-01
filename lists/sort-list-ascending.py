7019e6c09255556257abe01f542a459fc29ffcc8
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-09-01 15:16:43 +0800

    Create sort-list-ascending.py

---
+++ lists/sort-list-ascending.py
@@ -0,0 +1,18 @@
+"""
+7. List Sorting:
+Write a program that sorts a list of numbers in ascending order and prints the sorted list.
+"""
+
+list = [3,-32,102,31,63]
+
+sortedList = sorted(list)
+
+print("List:")
+for i in list:
+    print(i)
+
+print()
+
+print("Ascending Sorted List:")
+for i in sortedList:
+    print(i)
