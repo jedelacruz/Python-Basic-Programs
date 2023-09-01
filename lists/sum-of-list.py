de503f3716920d5fb1ef53292f17454a46805082
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-09-01 11:54:25 +0800

    Create sum-of-list.py

---
+++ lists/sum-of-list.py
@@ -0,0 +1,13 @@
+"""
+1. Sum of List Elements:
+Write a program that calculates and prints the sum of all elements in a given list.
+
+"""
+
+list = [20,30,40,50]
+sum = 0
+
+for num in list:
+    sum += num
+
+print("Sum is", sum)
