b1fb53b83a3284d5c3f23d4e2e3c404021f1b63d
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-09-01 12:00:54 +0800

    Create even-num-on-list.py

---
+++ lists/even-num-on-list.py
@@ -0,0 +1,10 @@
+"""
+4. Even Numbers in a List:
+Write a program that extracts and prints all the even numbers from a given list.
+"""
+
+list = [73, 87, 65, 10, 16, 100]
+
+for i in list:
+    if (i %2 == 0):
+        print(i)
