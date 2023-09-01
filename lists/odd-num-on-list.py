5657699e49ebcf8af94693648d73fe898cc7d3ec
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-09-01 12:01:10 +0800

    Create odd-num-on-list.py

---
+++ lists/odd-num-on-list.py
@@ -0,0 +1,10 @@
+"""
+4. Odd Numbers in a List:
+Write a program that extracts and prints all the odd numbers from a given list.
+"""
+
+list = [73, 87, 65, 10, 16, 100]
+
+for i in list:
+    if (i %2 != 0):
+        print(i)
