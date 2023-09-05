a68593367a4dfcd369ff157d7eee6ac0f74ab9f4
Je Dela Cruz <93860350+jedelacruz@users.noreply.github.com>
2023-09-05 09:22:28 +0800

    Update ToDoList.py

--- ToDoList.py
+++ ToDoList.py
@@ -1,32 +1,77 @@
-# to do app
-
-list = []
-
-print("To do App")
+taskList = []
 
 def main():
+    print("Task List Manager\n")
+    while True:
+        print("1. Add Task")
+        print("2. View Tasks")
+        print("3. Delete Task")
+        print("4. Quit")
+        print()
 
-    print("1. View List")
-    print("2. Add")
-    print("3. Delete")
-    print("4. Exit")
-    choice = int(input(": "))
+        try:
+            menuChoice = int(input("Enter a choice: "))
+            print()
+            if menuChoice == 1:
+                addTaskSec()
+            elif menuChoice == 2:
+                viewTaskSec()
+            elif menuChoice == 3:
+                deleteTaskSec()
+            elif menuChoice == 4:
+                break
+            else:
+                print("Invalid choice. Please select a valid option.")
+        except ValueError:
+            print("Invalid choice. Please enter a number.")
+        print()
 
-    if(choice == 1):
-        print(list)
-        main()
-    elif(choice == 2):
-        task = input("Enter text you want to add: ")
-        list.append(task)
-        main()
-    elif(choice == 3):
-        deleteTask = int(input("Enter the index/number you want to delete.(starts at 0) :"))
-        list.pop(deleteTask)
-        main()
-    elif(choice == 4):
-        print("Thanks for using!")
-        exit()
+
+def addTaskSec():
+    print("----- Add Task -----\n")
+    while True:
+        askUser = input("Enter task (or press Enter to return to the main menu): ")
+        print()
+        if not askUser:
+            print("Returning to the main menu.")
+            print()
+            break
+        else:
+            taskList.append(askUser)
+            print("Task added successfully.")
+            print()
+
+def viewTaskSec():
+    print("----- View Tasks -----\n")
+    if not taskList:
+        print("No tasks available.")
     else:
-        print("Invalid choice!")
-        main()
-main()
+        for index, task in enumerate(taskList, start=1):
+            print(f"{index}. {task}")
+    print()
+
+def deleteTaskSec():
+    print("----- Delete Task -----\n")
+    viewTaskSec()
+    if not taskList:
+        print("Task list is empty.")
+        return
+
+    while True:
+        try:
+            deleteAsk = int(input("Enter the number of the task to delete (or press Enter to return to the main menu): "))
+            print()
+            if 1 <= deleteAsk <= len(taskList):
+                deleted_task = taskList.pop(deleteAsk - 1)
+                print(f"Task '{deleted_task}' deleted successfully.")
+                break
+            elif not deleteAsk:
+                print("Returning to the main menu.\n")
+                break
+            else:
+                print("Invalid task number. Please enter a valid task number.")
+        except ValueError:
+            print("Invalid input. Please enter a number.")
+
+if __name__ == "__main__":
+    main()
